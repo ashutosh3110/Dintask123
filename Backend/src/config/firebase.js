1e531c9d89775f73914099173d7018f0bef02c0b
ashu3110 <ashutoshbankey21306@gmail.com>
2026-02-14 13:15:09 +0530

    ne fcm

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -1,8 +1,12 @@
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require(path.join(__dirname, '../../serviceAccountKey.json'));
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
 
+const serviceAccount = JSON.parse(
+  Buffer.from(base64, "base64").toString("utf-8")
+);
+console.log(serviceAccount);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
